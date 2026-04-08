5f0d1d042ee70ca8bf2381477af29f54f526e791
Hemanta Barik <barikhemantamarch@gmail.com>
2026-04-08 15:25:46 +0530

    Added explicit wait for login

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,14 +1,16 @@
 import pytest
 from pages.login_page import LoginPage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_valid_login(driver):
     login_page = LoginPage(driver)
 
-    # Step 1: Open site
     login_page.open_url()
-
-    # Step 2: Perform login
     login_page.login("Admin", "admin123")
 
-    # Step 3: Assertion
+    WebDriverWait(driver, 10).until(
+        EC.url_contains("dashboard")
+    )
+
     assert "dashboard" in driver.current_url
